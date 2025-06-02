3f0dd9143797f86f4e1bc3845511111d15581cc9
LawsOfScience <thesandwichsupreme@gmail.com>
2025-06-02 16:20:04 -0400

    Add beginnings of CLI

---
+++ src/main/kotlin/CarpoolCLIMain.kt
@@ -0,0 +1,45 @@
+import org.bread_experts_group.Flag
+import org.bread_experts_group.readArgs
+import org.bread_experts_group.logging.ColoredLogger
+import java.nio.file.Path
+import java.util.logging.Level
+import kotlin.io.path.exists
+import kotlin.io.path.notExists
+import kotlin.system.exitProcess
+
+val FLAGS = listOf(
+    Flag("log_level", "The logging level to use.", default = Level.WARNING),
+    Flag("start", "Whether to start the supervisor daemon.", default = false),
+    Flag("stop", "Whether to stop the supervisor daemon.", default = false),
+    Flag("socket", "The location of the socket used to talk with the supervisor daemon.", default = Path.of("./carpool.sock")),
+)
+val LOGGER = ColoredLogger.newLogger("ApplicationCarpool_CLI")
+
+fun main(args: Array<String>) {
+    LOGGER.info("- Reading arguments")
+
+    val (singleArgs, _) = readArgs(args, FLAGS, "Application Carpool", "Test")
+
+    LOGGER.level = singleArgs["log_level"] as Level
+    val start = singleArgs["start"] as Boolean
+    val socket = singleArgs["socket"] as Path
+
+    if (start)
+        spawnSupervisor(socket)
+    else if (socket.notExists()) {
+        LOGGER.severe("The supervisor daemon does not appear to be running. Please start it with -start.")
+        exitProcess(1)
+    }
+}
+
+fun spawnSupervisor(socketPath: Path) {
+    LOGGER.info("Attempting to start supervisor daemon...")
+    if (socketPath.exists()) {
+        LOGGER.severe("You have asked to start the supervisor daemon, but it appears to already be running.")
+        exitProcess(1)
+    }
+
+    // TODO: start supervisor daemon
+
+    LOGGER.info("Supervisor daemon started.")
+}
